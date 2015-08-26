6d23dd54134a90bbbffdbf4d7e6ac9e7369355f6
shajeers <shajeers@gmail.com>
2015-08-26 19:03:34 +0530

    Added Swagger + Swagger UI

--- elevateServices/src/main/java/services/ElevateRestService.java
+++ elevateServices/src/main/java/services/ElevateRestService.java
@@ -7,9 +7,13 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import main.java.model.RestResponse;
 
-
+@Api(value = "Elevate Rest Services")
 @Path("/showData")
 public class ElevateRestService {
 	
@@ -26,6 +30,9 @@ public class ElevateRestService {
 	// Sample URL : http://localhost:8080/elevateServices/rest/showData
 	@GET
 	 @Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Show Data",
+    notes = "Show Data Operation which returns a Hardcoded ID & Desc",
+    response = RestResponse.class)
 	public RestResponse showData2()
 	{
 		String result = "reached the Rest Service Desc";
@@ -38,7 +45,11 @@ public class ElevateRestService {
 	
 	@GET
 	@Path("/employee/{employeeId}/departmentId/{deptId}")
-	public Response showDataWithPathParams(@PathParam("employeeId") String employeeId,@PathParam("deptId") String deptId)
+	@ApiOperation(value = "Show Data Emp Dept",
+    notes = "Show Data Operation which returns a Hardcoded ID & Desc",
+    response = Response.class)	
+	public Response showDataWithPathParams(@ApiParam(value = "Employeed ID", required = true) @PathParam("employeeId") String employeeId,
+			@ApiParam(value = "Department ID", required = true) @PathParam("deptId") String deptId)
 	{
 		String result = "reached the Rest Service with employee Id : " + employeeId + "and dept id:" + deptId;
 		return Response.status(200).entity(result).build();	  
